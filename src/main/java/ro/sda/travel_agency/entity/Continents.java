331cecff8883e6ce5bb8c675a15a50eb47f82ca6
RebecaMoisuc <git.kafkanii@gmail.com>
2024-05-11 14:17:20 +0300

    travel agency

---
+++ src/main/java/ro/sda/travel_agency/entity/Continents.java
@@ -0,0 +1,22 @@
+package ro.sda.travel_agency.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "continents")
+public class Continents {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+
+}
